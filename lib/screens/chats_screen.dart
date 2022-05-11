a21256152b5e3ed982bb29c17c34c70b3be2fc20
Adam Taylor <tayloradam1999@gmail.com>
2022-05-11 13:00:24 -0500

    Task 0

---
+++ lib/screens/chats_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
