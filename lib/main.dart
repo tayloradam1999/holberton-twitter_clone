db46028df593a471ae2e61560f30e6e0354cbb51
Adam Taylor <tayloradam1999@gmail.com>
2022-05-11 13:25:58 -0500

    Task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
 import 'package:twitter/screens/signin_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:twitter/providers/app_state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Twitter Clone',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-        primaryColorDark: Colors.blue.shade800,
-      ),
-      home: const SignIn(),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+        ],
+        child: MaterialApp(
+          title: 'Twitter Clone',
+          theme: ThemeData(
+            primaryColor: Colors.blue,
+            primaryColorDark: Colors.blue.shade800,
+          ),
+          home: const SignIn(),
+        ));
   }
 }
